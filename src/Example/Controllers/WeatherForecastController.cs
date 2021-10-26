a51e0806fd27061306f7148bfe7d746ecc1853a9
Relson <relson.augusto@hotmail.com>
2021-10-26 15:25:43 -0300

    First Commit

---
+++ src/Example/Controllers/WeatherForecastController.cs
@@ -0,0 +1,75 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IAmazonDynamoDB _amazonDynamoDB;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IAmazonDynamoDB amazonDynamoDB)
+        {
+            _logger = logger;
+            _amazonDynamoDB = amazonDynamoDB;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var request = new CreateTableRequest
+            {
+                TableName = "test",
+                KeySchema = new List<KeySchemaElement>
+                {
+                    new KeySchemaElement
+                    {
+                        AttributeName = "Title",
+                        KeyType = "HASH"
+                    }
+                },
+                AttributeDefinitions = new List<AttributeDefinition>
+                {
+                    new AttributeDefinition
+                    {
+                        AttributeName = "Title",
+                        AttributeType = "S"
+                    }
+                },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 5,
+                    WriteCapacityUnits = 5
+                },
+                StreamSpecification = new StreamSpecification
+                {
+                    StreamEnabled = true,
+                    StreamViewType = StreamViewType.NEW_AND_OLD_IMAGES
+                }
+            };
+            Console.WriteLine(_amazonDynamoDB.Config.ServiceURL);
+            var response = await _amazonDynamoDB.CreateTableAsync(request);
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
